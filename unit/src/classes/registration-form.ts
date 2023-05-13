f05cf019e641df6ac7d060ac88f903548a0b30e5
Falometov <vadfalo@gmail.com>
2023-05-13 16:50:08 +0300

    created errors text constants

--- unit/src/classes/registration-form.ts
+++ unit/src/classes/registration-form.ts
@@ -1,14 +1,24 @@
+import { INVALID_USERNAME_ERROR } from "../utils/constants";
+import { usernameRegex } from "../utils/regular-expressions";
+
 export class RegistrationForm {
+  private username!: string;
+  private userAge!: string;
+  private userEmail!: string;
+  private userPhoneNumber!: string;
+  private userPassword!: string;
 
-    private userName!: string;
-    private userAge!: string;
-    private userEmail!: string;
-    private userPhoneNumber!: string;
-    private userPassword!: string;
+  constructor() {}
 
-    constructor(){}
+  get currentUsername() {
+    return this.username;
+  }
 
-    public setName(name: string){
-
-    }
+  public setName(username: string) {
+    if (usernameRegex.test(username)) this.username = username;
+    else throw new Error(INVALID_USERNAME_ERROR);
+  }
 }
+
+const rf = new RegistrationForm
+console.log(rf.currentUsername);
---
+++ unit/src/utils/constants.ts
@@ -0,0 +1,9 @@
+export const INVALID_USERNAME_ERROR =
+  "Invalid value. Username must be 3-15 characters long and contain only Latin letters";
+export const INVALID_AGE_ERROR =
+  "Invalid value. Age must a number between 16 and 99";
+export const INVALID_EMAIL_ERROR = "Invalid email format";
+export const INVALID_PHONE_NUMBER_ERROR =
+  "Invalid value. Phone number must must be in format +375XXXXXXXXX";
+export const INVALID_PASSWORD_ERROR =
+  "Invalid value. Password must be longer than 6 characters and contain lowercase and uppercase Latin letters, numbers and special characters";
