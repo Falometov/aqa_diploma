32cbbbba3a3812323bd6e561280e34b1692f8a92
Falometov <vadfalo@gmail.com>
2023-05-13 20:23:47 +0300

    created a lot of tests which check behavior of registration form

--- unit/src/classes/registration-form.ts
+++ unit/src/classes/registration-form.ts
@@ -1,11 +1,4 @@
-import {
-  INVALID_AGE_ERROR,
-  INVALID_EMAIL_ERROR,
-  INVALID_PASSWORD_ERROR,
-  INVALID_PHONE_NUMBER_ERROR,
-  INVALID_USERNAME_ERROR,
-  NONEXISTING_VALUE_ERROR,
-} from "../utils/constants";
+import { ERRORS, NONEXISTING_VALUE_ERROR } from "../utils/constants";
 import {
   ageRegex,
   emailRegex,
@@ -13,6 +6,7 @@ import {
   phoneNumberRegex,
   usernameRegex,
 } from "../utils/regular-expressions";
+import { Fields } from "../utils/types";
 
 export class RegistrationForm {
   private username!: string;
@@ -51,34 +45,34 @@ export class RegistrationForm {
   public setUsername(username: string): void {
     const matchStrings = username.match(usernameRegex);
     if (matchStrings && matchStrings[0] === username) this.username = username;
-    else throw new Error(INVALID_USERNAME_ERROR);
+    else throw new Error(ERRORS[Fields.Username]);
   }
 
   public setUserAge(age: string): void {
     const matchStrings = age.match(ageRegex);
     if (matchStrings && matchStrings[0] === age)
       if (Number(age) >= 16) this.userAge = age;
-      else throw new Error(INVALID_AGE_ERROR);
-    else throw new Error(INVALID_AGE_ERROR);
+      else throw new Error(ERRORS[Fields.Age]);
+    else throw new Error(ERRORS[Fields.Age]);
   }
 
   public setUserEmail(email: string): void {
     const matchStrings = email.match(emailRegex);
     if (matchStrings && matchStrings[0] === email) this.userEmail = email;
-    else throw new Error(INVALID_EMAIL_ERROR);
+    else throw new Error(ERRORS[Fields.Email]);
   }
 
   public setUserPhoneNumber(phoneNumber: string): void {
     const matchStrings = phoneNumber.match(phoneNumberRegex);
     if (matchStrings && matchStrings[0] === phoneNumber)
       this.userPhoneNumber = phoneNumber;
-    else throw new Error(INVALID_PHONE_NUMBER_ERROR);
+    else throw new Error(ERRORS[Fields.PhoneNumber]);
   }
 
   public setUserPassword(password: string): void {
     const matchStrings = password.match(passwordRegex);
     if (matchStrings && matchStrings[0] === password)
       this.userPassword = password;
-    else throw new Error(INVALID_PASSWORD_ERROR);
+    else throw new Error(ERRORS[Fields.Password]);
   }
 }
--- unit/tests/registration-form.spec.ts
+++ unit/tests/registration-form.spec.ts
@@ -1,14 +1,77 @@
 import { expect } from "chai";
 import { RegistrationForm } from "../src/classes/registration-form";
-import { validUsernames } from "../src/utils/constants";
+import {
+  ERRORS,
+  EXCEPTIONS_TESTS,
+  POSITIVE_TESTS,
+} from "../src/utils/constants";
+import { Fields } from "../src/utils/types";
 
 const registrationForm = new RegistrationForm();
 
 describe("Positive tests", () => {
-  validUsernames.forEach((username) => {
-    it(`Should correctly set "${username}" value in "Username" field`, () => {
-      registrationForm.setUsername(username);
-      expect(registrationForm.currentUsername).to.equal(username);
+  for (const valuesList in POSITIVE_TESTS) {
+    POSITIVE_TESTS[valuesList].forEach((value) => {
+      it(`Value "${value}" is correctly set as "${valuesList}" using "${valuesList}" field`, () => {
+        switch (valuesList) {
+          case Fields.Username:
+            registrationForm.setUsername(value);
+            expect(registrationForm.currentUsername).to.equal(value);
+            break;
+          case Fields.Age:
+            registrationForm.setUserAge(value);
+            expect(registrationForm.currentUserAge).to.equal(value);
+            break;
+          case Fields.Email:
+            registrationForm.setUserEmail(value);
+            expect(registrationForm.currentUserEmail).to.equal(value);
+            break;
+          case Fields.PhoneNumber:
+            registrationForm.setUserPhoneNumber(value);
+            expect(registrationForm.currentUserPhoneNumber).to.equal(value);
+            break;
+          case Fields.Password:
+            registrationForm.setUserPassword(value);
+            expect(registrationForm.currentUserPassword).to.equal(value);
+            break;
+        }
+      });
     });
-  });
+  }
+});
+
+describe("Exception tests", () => {
+  for (const valuesList in POSITIVE_TESTS) {
+    EXCEPTIONS_TESTS[valuesList].forEach((value) => {
+      it(`Setting "${value}" value as "${valuesList}" using "${valuesList}" field throws error`, () => {
+        switch (valuesList) {
+          case Fields.Username:
+            expect(() => registrationForm.setUsername(value)).to.throw(
+              ERRORS[Fields.Username]
+            );
+            break;
+          case Fields.Age:
+            expect(() => registrationForm.setUserAge(value)).to.throw(
+              ERRORS[Fields.Age]
+            );
+            break;
+          case Fields.Email:
+            expect(() => registrationForm.setUserEmail(value)).to.throw(
+              ERRORS[Fields.Email]
+            );
+            break;
+          case Fields.PhoneNumber:
+            expect(() => registrationForm.setUserPhoneNumber(value)).to.throw(
+              ERRORS[Fields.PhoneNumber]
+            );
+            break;
+          case Fields.Password:
+            expect(() => registrationForm.setUserPassword(value)).to.throw(
+              ERRORS[Fields.Password]
+            );
+            break;
+        }
+      });
+    });
+  }
 });
