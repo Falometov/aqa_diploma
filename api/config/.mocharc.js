83307f0ef6a8c3b9a1695008bca1019bd5670854
Falometov <vadfalo@gmail.com>
2023-05-17 11:32:25 +0300

    installed axios, configurated mocha, added script for run api tests

---
+++ api/config/.mocharc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  spec: "api/tests/**/*.spec.ts",
+  extension: "ts",
+  require: "ts-node/register",
+  reporter: 'mochawesome',
+  reporterOptions: [
+      'reportFilename=[datetime]_[status]_Diploma-API-Testing',
+      'json=false',
+      'html=true',
+      'overwrite=false',
+      'timestamp=longDate',
+      'consoleReporter=none',
+      'autoOpen=true',
+      'reportDir=./api/assets/mochawesome-report'
+  ],
+};
